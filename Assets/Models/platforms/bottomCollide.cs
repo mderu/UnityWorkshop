bee91d0682ab106bcc5c1285f91eebb52ddbf75a
Rica Feng <topgb@msn.com>
2014-12-05 15:56:47 -0800

    Added Calvin's Platforms
    Spike platforms
    Changed flag graphic to flag

---
+++ Assets/Models/platforms/bottomCollide.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bottomCollide : MonoBehaviour {
+
+	// Use this for initialization
+	public BoxCollider spikeCol;
+
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
